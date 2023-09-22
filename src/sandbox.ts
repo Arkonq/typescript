fa323ca85db4a7884204a418c3eecb984e034a96
Arkonq <arkonq@gmail.com>
2023-09-22 11:59:08 +0600

    Function Signatures

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,10 +1,24 @@
-type StrOrNum = string | number;
-type ObjWithName = {name: string, uid: StrOrNum}
+// let greet: Function;
 
-const logDetails = (uid: StrOrNum, item: string) => {
-  console.log(`${item} has a uid of ${uid}`);
+// example 1
+let greet: (anyName: string, putThere: string) => void; 
+
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
 }
 
-const greet = (user: ObjWithName) => {
-  console.log(`${user.name} says hello`);
+// example 2
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  return action === "add" ? numOne + numTwo : numOne - numTwo;
+}
+
+// example 3
+let logDetails: (anyNameForObject: {name: string, age: number}) => void;
+
+type Person = { name: string, age: number }
+
+logDetails = (user: Person) => {
+  console.log(`${user.name} is ${user.age} y.o.`);
 }
